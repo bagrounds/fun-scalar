adfe3ba22304255da34bb0bffbf4935425ae467b
bagrounds <bagrounds@gmail.com>
2017-05-22 00:39:21 -0700

    [PATCH][CHANGE] update to latest version of fun-test

--- test/index.js
+++ test/index.js
@@ -1,24 +1,19 @@
-#!/usr/bin/env node
 ;(function () {
   'use strict'
 
   /* imports */
   var tests = require('./tests')
   var runner = require('fun-test-runner')
-  var subject = require('../src')
+  var subject = require('..')
 
   main()
 
   function main () {
-    runner(tests, subject).fork(testError, id)
-  }
-
-  function testError (error) {
-    console.error('TEST_ERROR:' + error.message)
-  }
-
-  function id (x) {
-    return x
+    runner({ tests: tests, subject: subject }, function (error) {
+      if (error) {
+        throw error
+      }
+    })
   }
 })()
 
